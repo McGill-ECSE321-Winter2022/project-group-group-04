406a5f6b62099d0e3c3134f8faf3716e4b2e8c35
jedlamari <jed.lamari-saysset@mail.mcgill.ca>
2022-02-12 18:39:23 -0500

    Initial commit of the backend application, the gradle builds without issues

---
+++ Project321-Backend/src/main/java/ca/mcgill/ecse321/project321/Project321BackendApplication.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.project321;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class Project321BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project321BackendApplication.class, args);
+	}
+	@RequestMapping("/")
+  	public String greeting(){
+    	return "Hello world!";
+  	}
+
+}
