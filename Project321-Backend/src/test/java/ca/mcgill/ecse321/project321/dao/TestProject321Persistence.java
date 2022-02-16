a8fddd684439294486f8a2b9099263fea1b50771
Youjin <syjin311@yonsei.ac.kr>
2022-02-16 12:55:32 -0500

    TestPersistence_halfdone

--- Project321-Backend/src/test/java/ca/mcgill/ecse321/project321/dao/TestProject321Persistence.java
+++ Project321-Backend/src/test/java/ca/mcgill/ecse321/project321/dao/TestProject321Persistence.java
@@ -7,6 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.List;
+
+import ca.mcgill.ecse321.project321.model.Address;
+import ca.mcgill.ecse321.project321.model.Cart;
+import ca.mcgill.ecse321.project321.model.CartItem;
+import ca.mcgill.ecse321.project321.model.Customer;
+import ca.mcgill.ecse321.project321.model.Day;
+import ca.mcgill.ecse321.project321.model.Order;
+import ca.mcgill.ecse321.project321.model.Product;
+import ca.mcgill.ecse321.project321.model.StoreOwner;
+import ca.mcgill.ecse321.project321.model.TheGroceryStoreSystem;
+import ca.mcgill.ecse321.project321.model.TimeSlot;
+import ca.mcgill.ecse321.project321.model.Cart.ShoppingType;
+import ca.mcgill.ecse321.project321.model.Day.WeekDays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestProject321Persistence {
@@ -40,7 +62,7 @@ public class TestProject321Persistence {
 
 	@AfterEach
 	public void clearDatabase() {
-		// Fisrt, we clear registrations to avoid exceptions due to inconsistencies
+		// First, we clear registrations to avoid exceptions due to inconsistencies
 		orderRepository.deleteAll();
         shiftRepository.deleteAll();
         cartRepository.deleteAll();
@@ -55,7 +77,93 @@ public class TestProject321Persistence {
 	}
 
 	@Test
-    void contextLoads() {
+    public void testPersistAndLoadAddress() {
+		  String town = "TestTown";
+		  String street = "TestStreet";
+		  String postalCode = "TestPostalCode";
+		  int unit = 321;
+		  
+		  Address address = new Address(town,street,postalCode,unit);
+		  addressRepository.save(address);
+		  
+		  address = null;
+		  
+		  List<Address> addresslist = addressRepository.findByStreet(street);
+		  address = addresslist.get(0);
+		  assertNotNull(address);
+		  assertEquals(address.getTown(),town);
+		  assertEquals(address.getStreet(),street);
+		  assertEquals(address.getPostalCode(),postalCode);
+		  assertEquals(address.getUnit(),unit);
+		
+	}
+
+	@Test
+    public void testPersistAndLoadCartItem() {
+		String cartItemID = "cartItem321";
+		int quantity = 1;
+		Product product = new Product();
+		
+		CartItem cartItem = new CartItem(cartItemID, quantity,product);
+		cartItemRepository.save(cartItem);
+		
+		cartItem=null;
+		
+		List<CartItem> cartItemlist = cartItemRepository.findByProductAndQuantity(product, quantity);
+		cartItem = cartItemlist.get(0);
+		assertNotNull(cartItem);
+		assertEquals(cartItem.getCartItemID(),cartItemID);
+		assertEquals(cartItem.getQuantity(),quantity);
+		
+	}
+	
+	@Test
+    public void testPersistAndLoadCart() {
+		
+		  String cartID = "cart321";
+		  ShoppingType type = ShoppingType.Delivery;
+		  
+		  Customer customer = new Customer();
+		  TimeSlot timeSlot = new TimeSlot();
+		  Order order= new order();
+		  
+		  Cart cart = new Cart(cartID, type, customer, timeSlot, order);
+		  cartRepository.save(cart);
+		  
+		  cart = null;
+		  
+		  cart = cartRepository.findByOrder(order);
+		  assertNotNull(cart);
+		  assertEquals(cart.getCartID(),cartID);
+		  assertEquals(cart.getType(), type);
+		  
+	}
+	
+	@Test
+    public void testPersistAndLoadCustomer() {
+		
+		  String phone = "000-1111";
+		  
+		  String email = "customer@mail.com";
+		  String name = "TestCustomer";
+		  String password = "Testpassword";
+		  Address address = new Address();
+		  TheGroceryStoreSystem theGroceryStoreSystem = new TheGroceryStoreSystem ();
+		  
+		  Customer customer = new Customer(email, name, password, phone, address,  theGroceryStoreSystem);
+		  customerRepository.save(customer);
+		  
+		  customer = null;
+		  
+		  customer = customerRepository.findByEmail(email);
+		  assertNotNull(customer);
+		  assertEquals(customer.getPhone(),phone);
+		  assertEquals(customer.getAddress(),address);
+		  assertEquals(customer.getEmail(),email);
+		  assertEquals(customer.getPassword(),password);
+		  assertEquals(customer.getName(),name);
+		  
+
 	}
 
 }
