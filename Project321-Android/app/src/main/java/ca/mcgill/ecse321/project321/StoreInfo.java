024eb4975b06b146359f4f654a047eb24473e3e8
Cedric Barre <cedbarr@gmail.com>
2022-04-09 13:21:28 -0400

    Store Info page done with backend communication

--- Project321-Android/app/src/main/java/ca/mcgill/ecse321/project321/StoreInfo.java
+++ Project321-Android/app/src/main/java/ca/mcgill/ecse321/project321/StoreInfo.java
@@ -12,12 +12,12 @@ import androidx.fragment.app.Fragment;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import cz.msebera.android.httpclient.Header;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import ca.mcgill.ecse321.project321.databinding.StoreInfoBinding;
-import cz.msebera.android.httpclient.entity.mime.Header;
 
 public class StoreInfo extends Fragment {
 
@@ -44,24 +44,27 @@ public class StoreInfo extends Fragment {
         TextView ootf = (TextView) view.findViewById(R.id.storeoutoftownfee);
         TextView error = (TextView) view.findViewById(R.id.storeinfoerror);
         HttpUtils.get("store", new RequestParams(), new JsonHttpResponseHandler() {
+            @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 try {
                     String addressstr = new String();
                     email.setText(response.getString("email"));
                     phone.setText(response.getString("telephone"));
-                    addressstr = response.getString("unit") + " " +
-                            response.getString("street") + " " +
-                            response.getString("town") + " " +
-                            response.getString("postalCode");
+                    addressstr = response.getJSONObject("address").getString("unit") + " " +
+                            response.getJSONObject("address").getString("street") + " " +
+                            response.getJSONObject("address").getString("town") + " " +
+                            response.getJSONObject("address").getString("postalCode");
                     address.setText(addressstr);
-                    opening.setText(response.getString("openingHour"));
-                    closing.setText(response.getString("closingHour"));
-                    ootf.setText(response.getString("outOfTownFee"));
-                } catch (Exception e) {
+                    opening.setText("Opening hours : " + response.getString("openingHour"));
+                    closing.setText("Closing hours : " +response.getString("closingHour"));
+                    ootf.setText("Fee for delivery out of \ntown: " + response.getString("outOfTownFee") + "$");
+                } catch (JSONException e) {
+                    e.printStackTrace();
                     email.setText(response.toString());
                 }
                 error.setVisibility(View.GONE);
             }
+            @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 error.setText("Failed to get store info from the server.");
                 error.setVisibility(View.VISIBLE);
