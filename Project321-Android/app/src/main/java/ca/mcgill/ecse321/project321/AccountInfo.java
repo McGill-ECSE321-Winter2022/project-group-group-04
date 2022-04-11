489118f0a45f9139d3e8b54b115730b19b217839
karim-elg <karim.elgammal@mail.mcgill.ca>
2022-04-11 19:53:04 -0400

    account info xml done

---
+++ Project321-Android/app/src/main/java/ca/mcgill/ecse321/project321/AccountInfo.java
@@ -0,0 +1,22 @@
+package ca.mcgill.ecse321.project321;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.mcgill.ecse321.project321.databinding.AccountInfoBinding;
+
+public class AccountInfo extends Fragment {
+
+private AccountInfoBinding binding;
+Button update_button;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
